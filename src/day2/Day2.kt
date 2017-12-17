c54ff8ee14074eb848c33521aa70f2a0f469f854
josephoneill <devjoseph97@gmail.com>
2017-12-17 17:05:00 -0500

    Made local variables private

--- src/day2/Day2.kt
+++ src/day2/Day2.kt
@@ -6,14 +6,15 @@ import java.io.File
  * Created by Joseph on 12/17/2017
  */
 class Day2 {
-    var sum: Int = 0
+    private var sum: Int = 0
     fun init() {
         File("data/inputd2.txt").readLines(Charsets.UTF_8).forEach {
             sum += findDifference(it)
         }
         println(sum)
     }
-    fun findDifference(input : String) : Int {
+
+    private fun findDifference(input : String) : Int {
         var min = -1
         var max = -1
         input.split("\\s+".toRegex()).forEach {
--- src/day2/Day2P2.kt
+++ src/day2/Day2P2.kt
@@ -6,26 +6,29 @@ import java.io.File
  * Created by Joseph on 12/17/2017
  */
 class Day2P2 {
-    var sum: Int = 0
+    private var sum: Int = 0
     fun init() {
         File("data/inputd2.txt").readLines(Charsets.UTF_8).forEach {
-            sum += findDifference(it)
+            sum += findDivisionResult(it)
         }
         println(sum)
     }
-    fun findDifference(input : String) : Int {
-        var min = -1
-        var max = -1
-        input.split("\\s+".toRegex()).forEach {
-            val value = it.toInt()
-            if(min == -1 || value < min) {
-                min = value
-            }
-            if(max == -1 || value > max) {
-                max = value
+
+    private fun findDivisionResult(input: String): Int {
+        var value = 0
+        val inputList : List<String> = input.split("\\s+".toRegex())
+        for (i in 0 until inputList.size) {
+            val a = inputList[i].toInt()
+            for(j in i+1 until inputList.size) {
+                val b = inputList[j].toInt()
+                val remainder = if (a > b) a % b else b % a
+                if (remainder == 0) {
+                    value = if (a > b) a / b else b / a
+                    break
+                }
             }
         }
-        return max-min
+        return value
     }
 }
 
