35025215d63a965f10e445f0cb5ff3bf60260176
josephoneill <devjoseph97@gmail.com>
2017-12-17 16:23:46 -0500

    Fixed class issues

--- src/day2/Day2.kt
+++ src/day2/Day2.kt
@@ -13,21 +13,20 @@ class Day2 {
         }
         println(sum)
     }
-}
-
-fun findDifference(input : String) : Int {
-    var min = -1
-    var max = -1
-    input.split("\\s+".toRegex()).forEach {
-        val value = it.toInt()
-        if(min == -1 || value < min) {
-            min = value
+    fun findDifference(input : String) : Int {
+        var min = -1
+        var max = -1
+        input.split("\\s+".toRegex()).forEach {
+            val value = it.toInt()
+            if(min == -1 || value < min) {
+                min = value
+            }
+            if(max == -1 || value > max) {
+                max = value
+            }
         }
-        if(max == -1 || value > max) {
-            max = value
-        }
+        return max-min
     }
-    return max-min
 }
 
 fun main(args: Array<String>) {
