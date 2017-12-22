1b68b3228bc5f6cf9593232c3393826accbf8502
josephoneill <devjoseph97@gmail.com>
2017-12-21 20:11:28 -0500

    Solution to Day5

---
+++ src/day5/Day5.kt
@@ -0,0 +1,33 @@
+package day5
+
+import java.io.File
+
+/**
+ * Created by Joseph on 12/17/2017
+ */
+class Day5 {
+    var data : MutableList<Int> = mutableListOf()
+    fun init() {
+        File("data/inputd5.txt").readLines(Charsets.UTF_8).forEach {
+            data.add(it.toInt())
+        }
+        println(findNumOfJumps(data))
+    }
+
+    private fun findNumOfJumps(input: MutableList<Int>): Int {
+        var jumps = 0
+        var prevIndex = 0
+        var currIndex = 0
+        while(currIndex >= 0 && currIndex < input.size) {
+            currIndex = prevIndex + input[prevIndex]
+            input[prevIndex]++
+            jumps++
+            prevIndex = currIndex
+        }
+        return jumps
+    }
+}
+
+fun main(args: Array<String>) {
+    Day5().init()
+}
